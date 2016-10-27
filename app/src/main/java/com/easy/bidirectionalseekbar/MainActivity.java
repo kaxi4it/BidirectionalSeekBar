ba9b4f68fecb9a00265844cc2b7d61d52c6dbd2e
guyj <guyj@handsure.com>
2016-10-28 03:33:20 +0800

    滑动 回调 最小单位

--- app/src/main/java/com/easy/bidirectionalseekbar/MainActivity.java
+++ app/src/main/java/com/easy/bidirectionalseekbar/MainActivity.java
@@ -2,12 +2,23 @@ package com.easy.bidirectionalseekbar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    TextView textView;
+    BidirectionalSeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (BidirectionalSeekBar) findViewById(R.id.bSeekBar);
+        textView = (TextView) findViewById(R.id.text);
+        seekBar.setOnSeekBarChangeListener(new BidirectionalSeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(int leftProgress, int rightProgress) {
+                textView.setText("left=" + leftProgress + " right=" + rightProgress);
+            }
+        });
     }
 }
