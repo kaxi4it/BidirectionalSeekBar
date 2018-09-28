78fd738e30fb18d45d0e034b03f057c6582e4ad2
kaxi4it <gyj_android@sina.com>
2018-09-28 11:27:26 +0800

    修改demo 添加pop中效果

--- app/src/main/java/com/easy/bidirectionalseekbar/MainActivity.java
+++ app/src/main/java/com/easy/bidirectionalseekbar/MainActivity.java
@@ -1,7 +1,11 @@
 package com.easy.bidirectionalseekbar;
 
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.guyj.BidirectionalSeekBar;
@@ -30,5 +34,34 @@ public class MainActivity extends Activity {
                 textView2.setText("left=" + leftProgress + " right=" + rightProgress);
             }
         });
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View cView=View.inflate(MainActivity.this,R.layout.activity_main,null);
+                BidirectionalSeekBar seekBar3=cView.findViewById(R.id.bSeekBar1);
+                BidirectionalSeekBar seekBar4=cView.findViewById(R.id.bSeekBar2);
+                final TextView tv3=cView.findViewById(R.id.text1);
+                final TextView tv4=cView.findViewById(R.id.text2);
+                PopupWindow window=new PopupWindow(cView,getWindowManager().getDefaultDisplay().getWidth()-100,
+                        getWindowManager().getDefaultDisplay().getHeight()-100);
+                window.setBackgroundDrawable(new BitmapDrawable());
+                window.setOutsideTouchable(true);
+                window.setTouchable(true);
+                window.showAtLocation(textView1,Gravity.CENTER,0,0);
+                seekBar3.setOnSeekBarChangeListener(new BidirectionalSeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(int leftProgress, int rightProgress) {
+                        tv3.setText("left=" + leftProgress + " right=" + rightProgress);
+                    }
+                });
+                seekBar4.setOnSeekBarChangeListener(new BidirectionalSeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(int leftProgress, int rightProgress) {
+                        tv4.setText("left=" + leftProgress + " right=" + rightProgress);
+                    }
+                });
+            }
+        });
+
     }
 }
